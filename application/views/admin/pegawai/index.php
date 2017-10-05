1959aedd8378f557c5ef0040f3b2cc26cc97f853
ElfanRodh <elfanapoywali@gmail.com>
2017-10-05 11:03:25 +0700

    view awal

--- application/views/admin/pegawai/index.php
+++ application/views/admin/pegawai/index.php
@@ -4,47 +4,103 @@
 			<div class="pull-right tableTools-container"></div>
 		</div>
 		<div class="table-header">
-			LIST <?php echo $judul; ?>
+			Results for "Latest Registered Domains
 		</div>
 		<div>
 			<table id="dynamic-table" class="table table-striped table-bordered table-hover">
 				<thead>
 					<tr>
-						<th>No</th>
-						<th>Nama Pegawai</th>
-						<th>Alamat</th>
+						<th class="center">
+							<label class="pos-rel">
+								<input type="checkbox" class="ace" />
+								<span class="lbl"></span>
+							</label>
+						</th>
+						<th>Domain</th>
+						<th>Price</th>
+						<th class="hidden-480">Clicks</th>
+
 						<th>
 							<i class="ace-icon fa fa-clock-o bigger-110 hidden-480"></i>
-							Action
+							Update
 						</th>
+						<th class="hidden-480">Status</th>
+
+						<th></th>
 					</tr>
 				</thead>
 
 				<tbody>
-				<?php foreach ($pegawai as $row): ?>
 					<tr>
-						<td><?php echo $row->id_pegawai ?></td>
-						<td><?php echo $row->nama_pegawai ?></td>
-						<td><?php echo $row->alamat ?></td>
+						<td class="center">
+							<label class="pos-rel">
+								<input type="checkbox" class="ace" />
+								<span class="lbl"></span>
+							</label>
+						</td>
+
 						<td>
-							<a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-								<span class="blue">
-									<i class="ace-icon fa fa-search-plus bigger-200"></i>
-								</span>
-							</a>
-							<a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-								<span class="green">
-									<i class="ace-icon fa fa-pencil-square-o bigger-200"></i>
-								</span>
-							</a>
-							<a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-								<span class="red">
-									<i class="ace-icon fa fa-trash-o bigger-200"></i>
-								</span>
-							</a>
+							<a href="#">share.com</a>
+						</td>
+						<td>$30</td>
+						<td class="hidden-480">3,200</td>
+						<td>Feb 11</td>
+
+						<td class="hidden-480">
+							<span class="label label-sm label-info arrowed arrowed-righ">Sold</span>
+						</td>
+
+						<td>
+							<div class="hidden-sm hidden-xs action-buttons">
+								<a class="blue" href="#">
+									<i class="ace-icon fa fa-search-plus bigger-130"></i>
+								</a>
+
+								<a class="green" href="#">
+									<i class="ace-icon fa fa-pencil bigger-130"></i>
+								</a>
+
+								<a class="red" href="#">
+									<i class="ace-icon fa fa-trash-o bigger-130"></i>
+								</a>
+							</div>
+
+							<div class="hidden-md hidden-lg">
+								<div class="inline pos-rel">
+									<button class="btn btn-minier btn-yellow dropdown-toggle" data-toggle="dropdown" data-position="auto">
+										<i class="ace-icon fa fa-caret-down icon-only bigger-120"></i>
+									</button>
+
+									<ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+										<li>
+											<a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+												<span class="blue">
+													<i class="ace-icon fa fa-search-plus bigger-120"></i>
+												</span>
+											</a>
+										</li>
+
+										<li>
+											<a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+												<span class="green">
+													<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
+												</span>
+											</a>
+										</li>
+
+										<li>
+											<a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+												<span class="red">
+													<i class="ace-icon fa fa-trash-o bigger-120"></i>
+												</span>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
 						</td>
 					</tr>
-				<?php endforeach; ?>
+
 				</tbody>
 			</table>
 		</div>
